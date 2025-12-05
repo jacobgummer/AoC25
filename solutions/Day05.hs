18e5c0d7d5b2bb964de9ae35d62bf504dc4e6f9e
Jacob Siegumfeldt <jacobi7th@gmail.com>
2025-12-05 09:30:00 +0100

    Part 1 (day 5).

--- solutions/Day05.hs
+++ solutions/Day05.hs
@@ -1,22 +1,10 @@
-import Data.Heap (Heap)
-import qualified Data.Heap as H
-import Data.IntMap.Lazy (IntMap)
-import qualified Data.IntMap.Lazy as IntMap
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IntSet
-import Data.Map (Map)
-import qualified Data.Map as M
-import Data.Matrix hiding ((!))
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Text (Text)
-import qualified Data.Text as T
-import Data.Vector (Vector)
-import qualified Data.Vector as V
+import Data.Bifunctor (Bifunctor (bimap))
 import System.Environment (getArgs)
-import Utils.Grid.Matrix (CharGrid, Grid, GridPos, (!))
-import qualified Utils.Grid.Matrix as G
-import Utils.InputProcessing
+import Utils.InputProcessing (
+  breakLine,
+  parseInt,
+  readInputLines,
+ )
 
 type Solver = ProcessedInput -> Int
 
@@ -37,14 +25,16 @@ main = do
     ["p2", "-t"] -> runInput testFile part2
     _ -> putStrLn $ "Usage: ./day <" ++ day ++ "> <p1|p2> [-t]"
 
--- TODO: Adjust this.
-type ProcessedInput = [[Int]]
-
--- procesInputT :: [Text] -> ProcessedInput
--- procesInputT txts = undefined
+type ProcessedInput = ([(Int, Int)], [Int])
 
 procesInput :: [String] -> ProcessedInput
-procesInput strs = undefined
+procesInput strs = (ranges, ids)
+  where
+    ranges' = takeWhile (not . null) strs
+    ranges = map (bimap parseInt (parseInt . drop 1) . breakLine '-') ranges'
+
+    ids' = drop 1 $ dropWhile (not . null) strs
+    ids = map parseInt ids'
 
 runInput :: FilePath -> Solver -> IO ()
 runInput file part = do
@@ -52,8 +42,13 @@ runInput file part = do
   let input = procesInput raw
   print $ part input
 
+isBetween :: Int -> (Int, Int) -> Bool
+isBetween x (i, j) = i <= x && x <= j
+
 part1 :: Solver
-part1 input = length input
+part1 (ranges, ids) = length $ filter id areFresh
+  where
+    areFresh = map (\i -> any (i `isBetween`) ranges) ids
 
 part2 :: Solver
 part2 input = length input
