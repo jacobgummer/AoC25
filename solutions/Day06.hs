8484439e5da690b1893176f47543be7193c3d94b
Jacob Siegumfeldt <jacobi7th@gmail.com>
2025-12-06 12:17:35 +0100

    Part 2 (day 6).

--- solutions/Day06.hs
+++ solutions/Day06.hs
@@ -1,9 +1,20 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use head" #-}
 import Data.Bifunctor (Bifunctor (first))
-import Data.Matrix (Matrix (nrows), getRow, transpose)
+import Data.Char (isSpace)
+import Data.Matrix (
+  Matrix (ncols, nrows),
+  getCol,
+  getRow,
+  submatrix,
+  transpose,
+ )
 import Data.Maybe (fromMaybe)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 import System.Environment (getArgs)
+import Utils.Grid.Matrix (CharGrid)
 import qualified Utils.Grid.Matrix as G
 import Utils.InputProcessing (parseInt, readInputLines)
 
@@ -26,10 +37,11 @@ main = do
     ["p2", "-t"] -> runInput testFile part2
     _ -> putStrLn $ "Usage: ./day <" ++ day ++ "> <p1|p2> [-t]"
 
-type ProcessedInput = G.Grid String
+type ProcessedInput = (G.Grid String, CharGrid)
 
 procesInput :: [String] -> ProcessedInput
-procesInput = transpose . G.gridify . map words
+procesInput strs =
+  (transpose $ G.gridify $ map words strs, transpose $ G.gridify strs)
 
 runInput :: FilePath -> Solver -> IO ()
 runInput file part = do
@@ -43,7 +55,7 @@ operate "+" = V.sum
 operate _ = error "unexpected operator"
 
 part1 :: Solver
-part1 m = res
+part1 (m, _) = res
   where
     res = foldr checkRow 0 [1 .. nrows m]
 
@@ -53,5 +65,35 @@ part1 m = res
             first (V.map parseInt) $ fromMaybe undefined $ V.unsnoc row
        in acc + operate op vecNums
 
+trim :: String -> String
+trim = f . f
+  where
+    f = reverse . dropWhile isSpace
+
+getOperator :: String -> (Int -> Int -> Int)
+getOperator "+" = (+)
+getOperator "*" = (*)
+getOperator _ = undefined
+
 part2 :: Solver
-part2 m = length m
+part2 (_, m) = res' + lastCol
+  where
+    nRows = nrows m
+    nCols = ncols m
+    ops = words $ V.foldr (:) "" $ getCol nCols m
+    matNums = submatrix 1 nRows 1 (nCols - 1) m
+
+    (_, lastCol, res') =
+      foldl checkRow (0, if ops !! 0 == "+" then 0 else 1, 0) [1 .. nRows]
+
+    checkRow (i, colAcc, acc) row =
+      let rowVec = getRow row matNums
+          num' = trim $ V.foldr (:) "" rowVec
+          op = getOperator $ ops !! i
+       in if not $ null num'
+            then (i, colAcc `op` parseInt num', acc)
+            else
+              ( i + 1
+              , if ops !! (i + 1) == "+" then 0 else 1
+              , acc + colAcc
+              )
