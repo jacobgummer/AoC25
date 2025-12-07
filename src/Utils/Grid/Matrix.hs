227e79959f159cb3b8fc8aace2a48e42c4c0893f
Jacob Siegumfeldt <jacobi7th@gmail.com>
2025-12-07 21:53:46 +0100

    Refactor `Utils.Grid.Matrix`.

--- src/Utils/Grid/Matrix.hs
+++ src/Utils/Grid/Matrix.hs
@@ -1,17 +1,15 @@
 module Utils.Grid.Matrix (
   GridPos,
-  Grid,
+  Grid (..),
   CharGrid,
   gridify,
   gridifyFromText,
   cardinalNeighborsPos,
   diagonalNeighborsPos,
-  allPositions,
   allNeighborsPos,
   allNeighbors,
   renderCharGrid,
   (!),
-  bounds,
   inBounds,
   setGridElem,
 )
@@ -29,19 +27,50 @@ import Data.Text (Text, unpack)
 
 type GridPos = (Int, Int)
 
-type Grid a = Matrix a
+data Grid a = Grid
+  { gridNumRows :: Int
+  , gridNumCols :: Int
+  , gridMatrix :: Matrix a
+  , gridPositions :: [GridPos]
+  , gridBounds :: (Int, Int)
+  }
 
 type CharGrid = Grid Char
 
 gridify :: [[a]] -> Grid a
-gridify = fromLists
+gridify lsts =
+  Grid
+    { gridNumRows = m
+    , gridNumCols = n
+    , gridMatrix = grid
+    , gridPositions = [(i, j) | i <- [0 .. m - 1], j <- [0 .. n - 1]]
+    , gridBounds = (m, n)
+    }
+  where
+    grid = fromLists lsts
+    m = nrows grid
+    n = ncols grid
 
 gridifyFromText :: [Text] -> CharGrid
 gridifyFromText = gridify . map unpack
 
-allPositions :: Grid a -> [GridPos]
-allPositions grid =
-  let (m, n) = bounds grid in [(i, j) | i <- [0 .. m - 1], j <- [0 .. n - 1]]
+inBounds :: Grid a -> GridPos -> Bool
+inBounds grid (i, j) = i >= 0 && j >= 0 && i <= m - 1 && j <= n - 1
+  where
+    (m, n) = gridBounds grid
+
+renderCharGrid :: CharGrid -> String
+renderCharGrid (Grid {gridMatrix = g}) = prettyMatrix g
+
+allNeighbors :: Grid a -> GridPos -> [a]
+allNeighbors grid = map (grid !) . allNeighborsPos
+
+setGridElem :: a -> GridPos -> Grid a -> Grid a
+setGridElem elm (i, j) grid@(Grid {gridMatrix = g}) =
+  grid {gridMatrix = setElem elm (i + 1, j + 1) g}
+
+(!) :: Grid a -> GridPos -> a
+(!) (Grid {gridMatrix = g}) (i, j) = getElem (i + 1) (j + 1) g
 
 cardinalNeighborsPos :: GridPos -> [GridPos]
 cardinalNeighborsPos (i, j) = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
@@ -52,25 +81,3 @@ diagonalNeighborsPos (i, j) =
 
 allNeighborsPos :: GridPos -> [GridPos]
 allNeighborsPos p = cardinalNeighborsPos p ++ diagonalNeighborsPos p
-
-allNeighbors :: Grid a -> GridPos -> Grid a
-allNeighbors grid (i, j) =
-  let (m, n) = bounds grid
-   in submatrix (max 1 i) (min m (i + 2)) (max 1 j) (min n (j + 2)) grid
-
--- Zero-indexing.
-(!) :: Grid a -> GridPos -> a
-(!) grid (i, j) = getElem (i + 1) (j + 1) grid
-
-setGridElem :: a -> GridPos -> Grid a -> Grid a
-setGridElem elm (i, j) = setElem elm (i + 1, j + 1)
-
-bounds :: Grid a -> (Int, Int)
-bounds grid = (nrows grid, ncols grid)
-
-inBounds :: Grid a -> GridPos -> Bool
-inBounds grid (i, j) =
-  let (m, n) = bounds grid in i >= 0 && j >= 0 && i <= m - 1 && j <= n - 1
-
-renderCharGrid :: CharGrid -> String
-renderCharGrid = prettyMatrix
